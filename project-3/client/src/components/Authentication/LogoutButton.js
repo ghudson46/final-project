e69e1a5c46376e05ed33feda233912c71b313c30
Dianna Mack <dianna.mack@yahoo.com>
2020-11-19 17:54:39 -0600

    styled homepage

--- project-3/client/src/components/Authentication/LogoutButton.js
+++ project-3/client/src/components/Authentication/LogoutButton.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import './LogoutButton.css'
 
 function LogoutButton() {
     const { logout, isAuthenticated } = useAuth0();
