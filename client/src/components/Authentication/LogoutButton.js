007277df13d18d1a7879e3d7917ab5792880ac35
Garrett Hudson <garretthudson87@yahoo.com>
2020-11-25 11:33:04 -0500

    added logo, new name, and styled homepage

--- client/src/components/Authentication/LogoutButton.js
+++ client/src/components/Authentication/LogoutButton.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import './LogoutButton.css'
 
 function LogoutButton() {
     const { logout, isAuthenticated } = useAuth0();
     return (
         isAuthenticated && (
-            <div onClick={() => logout({
+            <button className="button" onClick={() => logout({
                 returnTo: window.location.origin,
             })}>
             Log Out
-        </div>
+        </button>
         )
     )
 }
