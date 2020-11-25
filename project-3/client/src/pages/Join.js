53e0c5f40baa16fcf0b6ea3d51ee419eeb6af559
Garrett Hudson <garretthudson87@yahoo.com>
2020-11-24 19:30:38 -0500

    allow user to search for existing room on join page, moved list of prior rooms to profile page

--- project-3/client/src/pages/Join.js
+++ project-3/client/src/pages/Join.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import API from '../utils/API';
-import RoomList from '../components/RoomList';
-import Room from '../components/Room';
 import { Link } from 'react-router-dom';
 
 // Sets user name and room name when user signs on
@@ -39,10 +37,14 @@ function Join(props) {
 
   const handleClick = (event) => {
     for (let i = 0; i < rooms.length; i++) {
-      if (!room || room !== rooms[i].name) {
+      if (!room) {
+        event.preventDefault();
+      }
+      if (room !== rooms[i].name) {
         event.preventDefault();
       } else {
-        console.log(rooms[i].name)
+        console.log(rooms[i].name);
+        window.location.replace(`http://localhost:3000/chat?name=${nickname}&room=${room}`);
       }
     }
   };
@@ -51,6 +53,7 @@ function Join(props) {
   return (
     <div>
         <div className="searchForm">
+        <h1>Search for an existing room</h1>
           <div>
             <input placeholder="Room" className="createInput mt-20" type="text" name="room" onChange={handleChange} />
           </div>
@@ -58,28 +61,7 @@ function Join(props) {
             <button className={'button mt-20'} type="submit">Search Room</button>
           </Link>
         </div>
-    
-        <h1>Join a past room</h1>
-        {rooms.length ? (
-          <RoomList>
-            {rooms.map(room => {
-              return (
-              room.userId == user.sub && (
-                <Room key={room._id}>
-                  <a href={`/chat?name=${nickname}&room=${room.name}`}>
-                    <strong>
-                      {room.name}
-                    </strong>
-                  </a>
-                </Room>
-                )
-              );
-            })}
-          </RoomList>
-        ) : (
-          <h3>No rooms to Display</h3>
-        )}
-  </div>
+    </div>
   );
 }
 
--- project-3/client/src/pages/Profile.js
+++ project-3/client/src/pages/Profile.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import RoomList from '../components/RoomList';
+import Room from '../components/Room';
+import API from '../utils/API';
 
 function Profile() {
+    const [rooms, setRooms] = useState([]);
+    const [room, setRoom] = useState('');
     const { user, isAuthenticated } = useAuth0();
+
+    let nickname; 
+    let userId = user.sub;
+
+    if (isAuthenticated && !user.given_name) {
+        nickname = user.nickname.replace(/[ ,.]/g, "");
+    } else if (isAuthenticated && user.given_name) {
+        nickname = user.given_name;
+    }
+
+    useEffect(() => {
+        loadRooms();
+      }, []);
+
+    function loadRooms() {
+        API.getRooms()
+          .then(res => {
+            setRooms(res.data);
+          }).catch(err => console.log(err));
+      };
+    
 
     return (
         isAuthenticated ? (
@@ -10,8 +36,27 @@ function Profile() {
             <img src={user.picture} alt={user.name} style={{borderRadius: '50%', height: '10rem', width: '10rem'}}/>
             <h2>{user.name}</h2>
             <p>{user.email}</p>
-            <button><a href="/join">Join a Room</a></button>
-            <button><a href="/create">Create a Room</a></button>
+
+            <h1>Your Rooms</h1>
+            {rooms.length ? (
+            <RoomList>
+                {rooms.map(room => {
+                return (
+                room.userId == user.sub && (
+                    <Room key={room._id}>
+                    <a href={`/chat?name=${nickname}&room=${room.name}`}>
+                        <strong>
+                        {room.name}
+                        </strong>
+                    </a>
+                    </Room>
+                    )
+                );
+                })}
+            </RoomList>
+            ) : (
+            <h3>No rooms to Display</h3>
+            )}
             
         </div>
         )
